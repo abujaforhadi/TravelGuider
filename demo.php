4502f801d7e1e832f38690cfd44a0c582aaef9d7
abujaforhadi <abujaforhadi@gmail.com>
2023-12-19 13:22:36 +0600

    aaaaaa

--- demo.php
+++ demo.php
@@ -1,33 +1,13 @@
-
 <?php
     ob_start();
     // include header.php file
     include ('header.php');
 ?>
 
-
 <?php
-
- 
-
-    /*  include top sale section */
-        //include ('Template/_top-sale.php');
-    /*  include top sale section */
-
-    /*  include special price section  */
-         include ('Template/_special-price1.php');
-    /*  include special price section  */
-
-
-
-    /*  include new phones section  */
-        //include ('Template/_new-phones.php');
-    /*  include new phones section  */
-
-
+    
+    include ('Template/_demo.php');
 ?>
-
-
 <?php
 // include footer.php file
 include ('footer.php');
